0cd6f4c213cef71bf7075623bb87e0f8bddce723
Habu-Kagumba <habukagumba@gmail.com>
2022-06-09 19:08:47 +0300

    Cleanup project:
    - Remove / update assets
    - Slightly update App structure and logo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
+import App from "./app";
+
+test("renders the logo", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logoElement = screen.getByAltText("logo");
+  expect(logoElement).toBeInTheDocument();
 });
